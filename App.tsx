44a7184099c483dde3fd592fabbe095e353f25b5
stiljanoruci <81534500+stiljanoruci@users.noreply.github.com>
2023-06-17 10:24:14 +0200

    Add files via upload
    buqeta ime app

---
+++ App.tsx
@@ -0,0 +1,24 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import AboutUs from './src/screens/AboutUs';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AboutUs" component={AboutUs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
